bf22b8f96e488aef6f8e9dfb02ee08a55173908d
showkatulislam <showkatul2@gmail.com>
2026-09-25 21:53:05 +0600

    feat: add global error handling

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import apiRouter from "./routes/index.js";
+import { notFoundMiddleware } from "./middlewares/not-found.middleware.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -26,6 +28,8 @@ app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(cookieParser());
 
 app.use("/api/v1", apiRouter);
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
 
 app.get("/health", (_req, res) => {
     res.status(200).json({
---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,26 @@
+import { ErrorRequestHandler } from "express";
+import { AppError } from "../errors/app-error.js";
+import { success } from "zod";
+import env from "../config/env.js";
+
+export const errorMiddleware:ErrorRequestHandler=(
+    error,
+    _req,
+    res,
+    _next
+):void=>{
+    if(error instanceof AppError){
+        res.status(error.statusCode).json({
+            success:false,
+            message:error.message,
+            errorCode:error.errorCode
+        })
+        return
+    }
+    console.error(error);
+
+    res.status(500).json({
+        success:false,
+        message:env.server.nodeEnv === "production"?"Internal server Error":error instanceof Error?error.message:"Internal server error"
+    })
+}
